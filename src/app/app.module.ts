6d1d065d9667f0b4b8f244181e0ec262b2b6245b
Jessica Lima <jsdl.9991@gmail.com>
2022-07-28 19:39:10 -0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componenentes/header/header.component';
 import { RodapeComponent } from './componenentes/rodape/rodape.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CadastroUsuarioComponent } from './pages/cadastro-usuario/cadastro-usuario.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './pages/usuario/login/login.component';
 import { MenuComponent } from './pages/menu/menu.component';
-import { CadastroProdutoComponent } from './pages/cadastro-produto/cadastro-produto.component';
-import { CadastroReceitaComponent } from './pages/cadastro-receita/cadastro-receita.component';
-import { ListagemProdutoComponent } from './pages/listagem-produto/listagem-produto.component';
-import { ListagemReceitaComponent } from './pages/listagem-receita/listagem-receita.component';
-import { FormusuarioComponent } from './pages/usuario/formusuario/formusuario.component';
 import { FormUsuarioComponent } from './pages/usuario/form-usuario/form-usuario.component';
-import { ListaProdutosComponent } from './pages/usuario/lista-produtos/lista-produtos.component';
+import { ListaProdutosComponent } from './pages/produtos/lista-produtos/lista-produtos.component';
 import { FormReceitasComponent } from './pages/receitas/form-receitas/form-receitas.component';
 import { ListaReceitasComponent } from './pages/receitas/lista-receitas/lista-receitas.component';
 import { FormProdutosComponent } from './pages/produtos/form-produtos/form-produtos.component';
@@ -26,14 +20,8 @@ import { FormProdutosComponent } from './pages/produtos/form-produtos/form-produ
     HeaderComponent,
     RodapeComponent,
     HomeComponent,
-    CadastroUsuarioComponent,
     LoginComponent,
     MenuComponent,
-    CadastroProdutoComponent,
-    CadastroReceitaComponent,
-    ListagemProdutoComponent,
-    ListagemReceitaComponent,
-    FormusuarioComponent,
     FormUsuarioComponent,
     ListaProdutosComponent,
     FormReceitasComponent,
